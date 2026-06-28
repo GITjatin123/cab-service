c537ad7a0bf67dbd33b889c743e4b57937d41f6c
Jatin Yadav <jatinyyadav.1999@gmail.com>
2026-06-28 19:38:32 +0530

    meta tag add

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Ludhiana Taxi Services">
+    <meta property="og:site_name" content="Ludhiana Taxi Services">
     <meta name="author" content="DynamicLayers">
     <base href="public/">
     <title>Ludhiana Taxi Services</title>
