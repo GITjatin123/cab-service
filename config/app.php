b9966e1e70d9cfb0d1953dd643e237ae0c2a5685
Jatin Yadav <jatinyyadav.1999@gmail.com>
2026-06-28 19:28:10 +0530

    config->app.php->app name update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Deep Cabs'),
+    'name' => env('APP_NAME', 'ludhiana taxi services'),
 
     /*
     |--------------------------------------------------------------------------
